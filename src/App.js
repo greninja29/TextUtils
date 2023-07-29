73dfe35d351026e9df5f0412bd8e1741382a5faa
Kalyan Reddy <kalyankally29@gmail.com>
2023-07-29 13:24:23 +0530

    rectified a route

--- src/App.js
+++ src/App.js
@@ -10,50 +10,50 @@ import {
   Routes,
 } from "react-router-dom";
 
-function App(){
-  const[mode ,setmode]=useState('light')
-  const[alert,setalert]=useState(null)
-  
-  const showalert=(messege,type)=>{
+function App() {
+  const [mode, setmode] = useState('light')
+  const [alert, setalert] = useState(null)
+
+  const showalert = (messege, type) => {
     setalert({
-      msg:messege,
-      type:type
+      msg: messege,
+      type: type
     })
     setTimeout(() => {
       setalert(null)
-    },2000);
+    }, 2000);
   }
 
-  let togglemode=()=>{
-    if(mode==='light'){
+  let togglemode = () => {
+    if (mode === 'light') {
       setmode('dark')
-      document.body.style.backgroundColor='#11293a'
-      showalert("Dark mode has been enabled","success")
+      document.body.style.backgroundColor = '#11293a'
+      showalert("Dark mode has been enabled", "success")
     }
-    else{
+    else {
       setmode('light')
-      document.body.style.backgroundColor='white'
-      showalert("Light mode has been enabled","success")      
+      document.body.style.backgroundColor = 'white'
+      showalert("Light mode has been enabled", "success")
     }
   }
-  return (  
-  <HashRouter>
-    {/* passed as variable */}
-    {/* <Navbar title={name} AboutText="About TextUtils"/> */}
-    {/* even though we donot pass enough props, if we define default props it gets added to the component*/}
-    {/* <Navbar /> */}
-    {/* <Navbar title="TextUtils" /> */}
-      <Navbar title="TextUtils" AboutText="AboutTextUtils" mode={mode} togglemode={togglemode}/>
-      <Alert alert={alert}/>
+  return (
+    <HashRouter>
+      {/* passed as variable */}
+      {/* <Navbar title={name} AboutText="About TextUtils"/> */}
+      {/* even though we donot pass enough props, if we define default props it gets added to the component*/}
+      {/* <Navbar /> */}
+      {/* <Navbar title="TextUtils" /> */}
+      <Navbar title="TextUtils" AboutText="AboutTextUtils" mode={mode} togglemode={togglemode} />
+      <Alert alert={alert} />
       {/* <TextForm showalert={showalert} heading="Enter Your Text" mode={mode}/> */}
       <div className="container my-3">
         <Routes>
-          <Route path='/' element={<TextForm showalert={showalert} heading="Enter Your Text" mode={mode}/>}/>
-          {/* <Route path='/TextUtils' element={<TextForm showalert={showalert} heading="Enter Your Text" mode={mode}/>}/> */}
-          <Route path='/abouttext' element={<Abouttext mode={mode}/>}/>
-        </Routes> 
+          <Route path='/' element={<TextForm showalert={showalert} heading="Enter Your Text" mode={mode} />} />
+          {/* <Route path='/TextUtils' element={<TextForm showalert={showalert} heading="Enter Your  Text" mode={mode}/>}/> */}
+          <Route path='/abouttext' element={<Abouttext mode={mode} />} />
+        </Routes>
       </div>
-      </HashRouter> 
+    </HashRouter>
   );
 }
 export default App;
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-export default function TextForm(props){
-  let handleupclick = ()=>{
+export default function TextForm(props) {
+  let handleupclick = () => {
     console.log("button is clicked");
     // let newtext=text.toUpperCase();
     // settext(newtext);
     let newone = "";
-    for (let i = 0; i < text.length; i++){
+    for (let i = 0; i < text.length; i++) {
       newone += String.fromCharCode(
         text.charCodeAt(i) > 96 && text.charCodeAt(i) < 123
           ? text.charCodeAt(i) - 32
@@ -14,7 +14,7 @@ export default function TextForm(props){
       // "String.fromCharCode(text.charCodeAt(i)-32)" is returning a string that is why we are unable to do like newone[i]=String.fromCharCode(text.charCodeAt(i)-32) by giving newone=text initially, and there is no char datatype in javascript
     }
     settext(newone);
-    props.showalert("converted to upper case","success")
+    props.showalert("converted to upper case", "success")
   };
   let handleloclick = () => {
     console.log("button is clicked");
@@ -30,7 +30,7 @@ export default function TextForm(props){
       // "String.fromCharCode(text.charCodeAt(i)-32)" is returning a string that is why we are unable to do like newone[i]=String.fromCharCode(text.charCodeAt(i)-32) by giving newone=text initially, and there is no char datatype in javascript
     }
     settext(newone);
-    props.showalert("converted to Lower case","success")    
+    props.showalert("converted to Lower case", "success")
   };
   // var word_count=1;
   let handlechange = (event) => {
@@ -56,11 +56,11 @@ export default function TextForm(props){
   let handleExtraSpaces = () => {
     let newtext = text.split(/[ ]+/); //used regular expression, all those words which are separated by one or more spaces are splitted into an array
     settext(newtext.join(" ")); // here all the words which are splitted are joined with single space, and there will be a space at the end also
-    props.showalert("Extra spaces Removed","success")  
+    props.showalert("Extra spaces Removed", "success")
   };
   let handleClearText = () => {
     settext("");
-    props.showalert("Text cleared","success")    
+    props.showalert("Text cleared", "success")
   };
   let handleCopying = () => {
     console.log("Text Copied");
@@ -68,7 +68,7 @@ export default function TextForm(props){
     test.select();
     test.setSelectionRange(0, 9999);
     navigator.clipboard.writeText(test.value);
-    props.showalert("Text copied to clipboard","success")    
+    props.showalert("Text copied to clipboard", "success")
   };
   let handleCapitalising = () => {
     console.log("capitalising button clicked"); //for debugging
@@ -101,14 +101,14 @@ export default function TextForm(props){
       justcheck[i] = temp;
     }
     settext(justcheck.join("\n"));
-    props.showalert("Text Capitalized","success")    
+    props.showalert("Text Capitalized", "success")
   };
   const [text, settext] = useState("");
   return (
     <>
       <div
         className="container"
-        style={{color:props.mode === "dark" ? "white" : "black" }}
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
       >
         <h1>{props.heading}</h1>
         <div className="mb-3">
